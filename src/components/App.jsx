f1b0dc6ac02eea70c1d5629faa5593ae823ba716
Riadh Korchid <riadhkourchid@gmail.com>
2022-11-14 22:14:01 +0100

    make home section responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,5 +8,6 @@ export default function () {
     return (<>
 <Nav/>
 <Home/>
+
     </>)
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import pic from "./riadhpic.png"
 export default function () {
     return (<>
-        <section id="home" >  
+        <section id="home" >
             <div className="section-left">
-                <h1>I'm <span>Riadh Korchid</span></h1>
-                <h2><span>Frontend</span> Developer</h2>
-                <p>I will help you create an amazing website for your business!</p>
-                <div className="portfolio">Portfolio</div>
+                <div className="container">
+                    <h1>I'm <span>Riadh Korchid</span></h1>
+                    <h2><span>Frontend</span> Developer</h2>
+                    <p>I will help you create an amazing website for your business!</p>
+                    <div className="portfolio">Portfolio</div>
+                </div>
+                <div>
+                    
+                </div>
+
             </div>
             <div className="section-right">
                 <img src={pic} />
